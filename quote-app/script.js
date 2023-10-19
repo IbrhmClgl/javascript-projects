534af7972b556719476b09578f1590463a555714
Ibrahim Ciloglu <ibrahimciloglu.ic@gmail.com>
2023-10-19 10:09:29 +0200

    feat: implement app logic

---
+++ quote-app/script.js
@@ -0,0 +1,15 @@
+const buttonEl = document.querySelector("button");
+const quoteEl = document.querySelector(".quote");
+const authorEl = document.querySelector(".author");
+
+buttonEl.addEventListener("click", () => {
+  quoteEl.innerHTML = "";
+  authorEl.innerHTML = "";
+
+  fetch("https://dummy-apis.netlify.app/api/quote").then((response) => {
+    return response.json().then((data) => {
+      quoteEl.append(document.createTextNode(data.quote));
+      authorEl.append(document.createTextNode(data.author));
+    });
+  });
+});
