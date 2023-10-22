70421a9edaa3e74e8036f2a26e5b40406a26d5fe
Ibrahim Ciloglu <ibrahimciloglu.ic@gmail.com>
2023-10-22 12:07:11 +0200

    feat: button for color mixer app

--- color-mixer/script.js
+++ color-mixer/script.js
@@ -4,6 +4,7 @@ const red = document.querySelector(".red");
 const green = document.querySelector(".green");
 const blue = document.querySelector(".blue");
 const rgb = document.querySelector(".rgb");
+const buttonEl = document.querySelector("button");
 
 function colorUpdate() {
   document.body.style.backgroundColor = `rgb(${red.value},${green.value},${blue.value})`;
@@ -11,3 +12,14 @@ function colorUpdate() {
 }
 
 document.addEventListener("input", colorUpdate);
+
+buttonEl.addEventListener("click", () => {
+  fetch("https://dummy-apis.netlify.app/api/color")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      document.body.style.backgroundColor = data.color;
+      rgb.textContent = data.color;
+    });
+});
--- pig-game/script.js
+++ pig-game/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0 = document.querySelector("#score--0");
 const score1 = document.querySelector("#score--1");
 const current0El = document.getElementById("current--0");
@@ -13,7 +15,9 @@ score0.textContent = 0;
 score1.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 btnRoll.addEventListener("click", () => {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -24,6 +28,13 @@ btnRoll.addEventListener("click", () => {
 
   if (dice !== 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
